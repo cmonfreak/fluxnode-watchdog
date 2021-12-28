06fdd41011cd53b70b6f170a48ffcc5b4067aae2
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2021-12-28 09:52:29 +0100

    removed zelcore explorer , added random check

--- watchdog.js
+++ watchdog.js
@@ -38,6 +38,12 @@ var kda_sleep=0;
 var after_fix=0;
 var sleep_msg=0;
 
+function between(min, max) {  
+  return Math.floor(
+    Math.random() * (max - min) + min
+  )
+}
+
 async function job_creator(){
 
   ++job_count;
@@ -469,13 +475,13 @@ function max() {
 
 async function Check_Sync(height,time) {
 
-  var exec_comment1=`curl -sk -m 8 https://explorer.flux.zelcore.io/api/status?q=getInfo | jq '.info.blocks'`
+  // var exec_comment1=`curl -sk -m 8 https://explorer.flux.zelcore.io/api/status?q=getInfo | jq '.info.blocks'`
   var exec_comment2=`curl -sk -m 8 https://explorer.runonflux.io/api/status?q=getInfo | jq '.info.blocks'`
   var exec_comment3=`curl -sk -m 8 https://explorer.zelcash.online/api/status?q=getInfo | jq '.info.blocks'`
-  var explorer_block_height_01 = await shell.exec(`${exec_comment1}`,{ silent: true }).stdout;
+ // var explorer_block_height_01 = await shell.exec(`${exec_comment1}`,{ silent: true }).stdout;
   var explorer_block_height_02 = await shell.exec(`${exec_comment2}`,{ silent: true }).stdout;
   var explorer_block_height_03 = await shell.exec(`${exec_comment3}`,{ silent: true }).stdout;
-  var explorer_block_height = max(explorer_block_height_01,explorer_block_height_02,explorer_block_height_03);
+  var explorer_block_height = max(explorer_block_height_02,explorer_block_height_03);
   var height_diff = Math.abs(explorer_block_height-height);
 
   if ( explorer_block_height == 0 ) {
@@ -1632,7 +1638,10 @@ tire_lock=0;
 
 
  if ( zelcash_height != "" && typeof zelcash_height != "undefined" ){
+   
+   
   await Check_Sync(zelcash_height,data_time_utc);
+   
  }
 
 
